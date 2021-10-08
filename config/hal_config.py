2760b19507ac42b845d2753b9f9bab7495d923fb
Hannu Henttinen <hannu.2.henttinen@samk.fi>
2021-10-08 13:13:29 +0300

    Naming convention

--- config/hal_config.py
+++ config/hal_config.py
@@ -10,14 +10,20 @@ with open(hal_config, "r") as stream:
 		xml_o = '<masters>\n'
 		xml_o += '\t<master idx="{masteridx}" appTimePeriod="{apptimeperiod}" refClockSyncCycles="{refclocksynccycles}">\n'
 		
-		for count,name in enumerate(data['ETHERCAT']['SLAVES']):
-			xml_o += '\t\t<slave idx="{count}" type="{name}" />\n'.format(count=count, name=name)
+		for count,type in enumerate(data['ETHERCAT']['SLAVES']):
+
+			if isinstance(type,dict):
+				terminal_type = list(type.keys())[0]
+				terminal_name = type[terminal_type]['name']
+				xml_o += '\t\t<slave idx="{count}" type="{type}" name="{name} />\n'.format(count=count,type=terminal_type,name=terminal_name)
+			else:
+				xml_o += '\t\t<slave idx="{count}" type="{type}" />\n'.format(count=count, type=type)
 
 		xml_o += '\t</master>\n</masters>'
 		xml_o = xml_o.format(masteridx=data['ETHERCAT']['MASTERIDX'], apptimeperiod=data['ETHERCAT']['APPTIMEPERIOD'], refclocksynccycles=data['ETHERCAT']['REFCLOCKSYNCCYCLES'])
 		print(xml_o)
 		f = open('hal.xml', 'w')
-		f.write(xml_o)
+		# f.write(xml_o)
 		f.close()
 	except yaml.YAMLError as exc:
 		print(exc)
