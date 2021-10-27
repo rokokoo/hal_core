40e3e2c7cfc8263a84835451617bf8a89fb447bf
Hannu Henttinen <hannu.2.henttinen@samk.fi>
2021-10-27 12:11:06 +0300

    Updated pythons

--- config/hal_config.py
+++ config/hal_config.py
@@ -1,58 +1,68 @@
 #!/usr/bin/env python3
 
-import yaml, collections
+import yaml
+import collections
+
 
 def parseModParam(modParam):
-	xml_o = ""
-	for param in modParam:
-		parName = list(param.keys())[0]
-		parVal = param[parName]
-		xml_o += '\n\t\t\t<modParam name="{}" value="{}" />'.format(parName,parVal)
+    xml_o = ""
+    for param in modParam:
+        parName = list(param.keys())[0]
+        parVal = param[parName]
+        xml_o += '\n\t\t\t<modParam name="{}" value="{}" />'.format(
+            parName, parVal)
 
-	return xml_o
+    return xml_o
+
 
 def parseTerminal(idx, terminal):
-	xml_o = '\n\t\t<slave' 
-	if isinstance(terminal,dict):
-		term_type = list(terminal.keys())[0]
-		xml_o += ' idx="{}" type="{}"'.format(idx,term_type)
+    xml_o = '\n\t\t<slave'
+    if isinstance(terminal, dict):
+        term_type = list(terminal.keys())[0]
+        xml_o += ' idx="{}" type="{}"'.format(idx, term_type)
 
-		term = terminal[term_type]
+        term = terminal[term_type]
+        print(len(term))
+        for i in term:
+            keys = i.keys()
+            # print(keys)
+            if 'name' in keys:
+                name = i['name']
+                xml_o += ' name="{}"'.format(i['name'])
+            else:
+                xml_o += '>'
 
-		for i in term:
-			keys = i.keys()
-			# print(keys)
-			if 'name' in keys:
-				name = i['name'] 
-				xml_o += ' name="{}"'.format(i['name'])
-			
-			if 'modParam' in keys:
-				xml_o += parseModParam(i['modParam'])
-			else:
-				xml_o += '>'
-		xml_o += '\n\t\t</slave>'
-	else:
-		xml_o += ' idx="{}" type="{}" />'.format(idx,terminal)
+            if 'modParam' in keys:
+                xml_o += parseModParam(i['modParam'])
+            else:
+                xml_o += '>'
 
-	return xml_o
+        xml_o += '\n\t\t</slave>'
+    else:
+        xml_o += ' idx="{}" type="{}" />'.format(idx, terminal)
+
+    return xml_o
+
 
 hal_config = "hal.yaml"
 
 with open(hal_config, "r") as stream:
-	try:
-		data = yaml.safe_load(stream)
+    try:
+        data = yaml.safe_load(stream)
 
-		xml_o = '<masters>\n'
-		xml_o += '\t<master idx="{masteridx}" appTimePeriod="{apptimeperiod}" refClockSyncCycles="{refclocksynccycles}">'
-		
-		for count,terminal in enumerate(data['ETHERCAT']['SLAVES']):
-			xml_o += parseTerminal(count, terminal)
+        xml_o = '<masters>\n'
+        xml_o += '\t<master idx="{masteridx}" appTimePeriod="{apptimeperiod}" refClockSyncCycles="{refclocksynccycles}">'
 
-		xml_o += '\n\t</master>\n</masters>'
-		xml_o = xml_o.format(masteridx=data['ETHERCAT']['MASTERIDX'], apptimeperiod=data['ETHERCAT']['APPTIMEPERIOD'], refclocksynccycles=data['ETHERCAT']['REFCLOCKSYNCCYCLES'])
-		# print(xml_o)
-		f = open('hal.xml', 'w')
-		f.write(xml_o)
-		f.close()
-	except yaml.YAMLError as exc:
-		print(exc)
+        for count, terminal in enumerate(data['hal']['slaves']):
+            xml_o += parseTerminal(count, terminal)
+
+        xml_o += '\n\t</master>\n</masters>'
+        master = data['hal']['master']
+        xml_o = xml_o.format(
+            masteridx=master['idx'], apptimeperiod=master['apptimeperiod'], refclocksynccycles=master['refclocksync'])
+        # print(xml_o)
+        f = open('hal.xml', 'w')
+        f.write(xml_o)
+        f.close()
+    except yaml.YAMLError as exc:
+        print(exc)
--- ros_ws/src/icim_msgs/src/populate.py
+++ ros_ws/src/icim_msgs/src/populate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from yaml.loader import SafeLoader
-import rospy, icim_msgs.msg, time, argparse
+import rospy
+import icim_msgs.msg
+import time
+import argparse
 from std_msgs.msg import String
 
 # def talker():
@@ -8,61 +11,65 @@ from std_msgs.msg import String
 #     rospy.init_node('talker', anonymous=True)
 #     rate = rospy.Rate(100) # 10000hz
 #     while not rospy.is_shutdown():
-#         information = subprocess.run(['halcmd','getp','lcec.0.7.enc-0-pos'], capture_output=True).stdout.decode('utf-8').strip() 
+#         information = subprocess.run(['halcmd','getp','lcec.0.7.enc-0-pos'], capture_output=True).stdout.decode('utf-8').strip()
 #         hello_str = "hello world %s" % rospy.get_time()
 #         rospy.loginfo(information)
 #         pub.publish(information)
 #         rate.sleep()
 
+
 class HALcmdPopulator():
-	def __init__(self, param_name) -> None:
-		params = rospy.get_param(param_name)
+    def __init__(self, param_name) -> None:
+        params = rospy.get_param(param_name)
 
-		self.publishers = []
-		self.mod = getattr(__import__('icim_msgs'),'msg')
+        self.publishers = []
+        self.mod = getattr(__import__('icim_msgs'), 'msg')
 
-		# Going through the YAML slaves
-		for count, terminal in enumerate(params['slaves']):
-			if isinstance(terminal, dict):
-				term_type = list(terminal.keys())[0]
-				# print(term_type)
-				name = None
-				for i in terminal[term_type]:
-					keys = i.keys()
-					if 'name' in keys:
-						name = i['name']
+        # Going through the YAML slaves
+        for count, terminal in enumerate(params['slaves']):
+            if isinstance(terminal, dict):
+                term_type = list(terminal.keys())[0]
+                # print(term_type)
+                name = None
+                for i in terminal[term_type]:
+                    keys = i.keys()
+                    if 'name' in keys:
+                        name = i['name']
 
-				if name is None:
-					self.topic_name = '/icim/terminal_{:02d}/{}'.format(count,term_type)
-				else:
-					self.topic_name = '/icim/terminal_{:02d}/{}'.format(count,name)
+                if name is None:
+                    self.topic_name = '/icim/terminal_{:02d}/{}'.format(
+                        count, term_type)
+                else:
+                    self.topic_name = '/icim/terminal_{:02d}/{}'.format(
+                        count, name)
 
-				msg_type = getattr(self.mod,term_type)
+                msg_type = getattr(self.mod, term_type)
 
-			else:
-				self.topic_name = '/icim/terminal_{:02d}/{}'.format(count, terminal)
-				msg_type = getattr(self.mod, terminal)
-				
-			publisher = rospy.Publisher(self.topic_name, msg_type, queue_size=1)
-			self.publishers.append(publisher)
-			
-		
+            else:
+                self.topic_name = '/icim/terminal_{:02d}/{}'.format(
+                    count, terminal)
+                msg_type = getattr(self.mod, terminal)
+
+            publisher = rospy.Publisher(
+                self.topic_name, msg_type, queue_size=1)
+            self.publishers.append(publisher)
+
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('-param', default='hal')
+    parser.add_argument('-param', default='hal')
 
-	args,_ = parser.parse_known_args()
+    args, _ = parser.parse_known_args()
 
-	rospy.init_node('icim_populator')
+    rospy.init_node('icim_populator')
 
-	if rospy.has_param(args.param):
-		populator = HALcmdPopulator(args.param)
+    if rospy.has_param(args.param):
+        populator = HALcmdPopulator(args.param)
 
-		r = rospy.Rate(1)
-		while not rospy.is_shutdown():
-			r.sleep()
-	else:
-		rospy.logerr('Param {param} was not set!'.format(param=args.hal))
+        r = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            r.sleep()
+    else:
+        rospy.logerr('Param {param} was not set!'.format(param=args.hal))
